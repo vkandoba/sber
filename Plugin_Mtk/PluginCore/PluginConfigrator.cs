49c1d7d8cba5747691e94f54ed8b277452e4e68b
vkandoba <vkandoba@skbkontur.ru>
2016-11-09 18:32:10 +0500

    Отключение строгой проверки заголовков при использовании стандатного WebClient'а

--- Plugin_Mtk/PluginCore/PluginConfigrator.cs
+++ Plugin_Mtk/PluginCore/PluginConfigrator.cs
@@ -14,10 +14,15 @@ namespace PluginCore
             return SetUnsafeHeaderParsingBase(false);
         }
 
+        /// <summary>
+        /// Способ выставить через Reflection настройку, определяемую в app.config
+        ///       <httpWebRequest useUnsafeHeaderParsing = "true" />
+        /// т. к. app.config приложения, куда загружается плагин, недостапун
+        /// </summary>
         private static bool SetUnsafeHeaderParsingBase(bool value)
         {
             var configSettingsType = typeof(System.Net.Configuration.SettingsSection);
-            var settingsType = Assembly.GetAssembly(configSettingsType)?.GetType(configSettingsType.Name);
+            var settingsType = Assembly.GetAssembly(configSettingsType)?.GetType($"{configSettingsType.FullName}Internal");
 
             object sectionProperty = settingsType?.InvokeMember("Section", 
                                                                 BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.NonPublic, 
